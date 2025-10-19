d65fb214e2ba39d66569db44cfbae0718d03aa92
rzwei <11663545+not-rzwei@users.noreply.github.com>
2025-10-19 19:24:43 +0900

    feat: round robin screenshot resizing

--- module/device/screenshot.py
+++ module/device/screenshot.py
@@ -28,9 +28,33 @@ class Screenshot(Adb, WSA, DroidCast, AScreenCap, Scrcpy, NemuIpc, LDOpenGL):
     _minicap_uninstalled = False
     _screenshot_interval = Timer(0.1)
     _last_save_time = {}
+    _resize_method_idx = 0
     image: np.ndarray
 
-    interpolation_methods = [cv2.INTER_AREA, cv2.INTER_LANCZOS4]
+    cv_interpolation_methods = [cv2.INTER_LANCZOS4, cv2.INTER_AREA]
+    pillow_interpolation_methods = [Image.LANCZOS, Image.BICUBIC]
+
+    @cached_property
+    def _resize_methods_list(self):
+        from itertools import zip_longest
+
+        cv_methods = [('cv', m) for m in self.cv_interpolation_methods]
+        pillow_methods = [('pillow', m) for m in self.pillow_interpolation_methods]
+
+        result = []
+        for cv_method, pillow_method in zip_longest(cv_methods, pillow_methods, fillvalue=None):
+            if cv_method:
+                result.append(cv_method)
+            if pillow_method:
+                result.append(pillow_method)
+
+        return result
+
+    def get_next_resize_method(self):
+        """Get the next resize method in round-robin fashion."""
+        method_type, interp = self._resize_methods_list[self._resize_method_idx]
+        self._resize_method_idx = (self._resize_method_idx + 1) % len(self._resize_methods_list)
+        return method_type, interp
 
     @cached_property
     def screenshot_methods(self):
@@ -69,7 +93,17 @@ class Screenshot(Adb, WSA, DroidCast, AScreenCap, Scrcpy, NemuIpc, LDOpenGL):
             self.image = method()
             width, height = image_size(self.image)
             if width != 1280 or height != 720:
-                self.image = cv2.resize(self.image, (1280, 720), interpolation=random.choice(self.interpolation_methods))
+                method_type, interp = self.get_next_resize_method()
+
+                if method_type == 'cv':
+                    self.image = cv2.resize(self.image, (1280, 720), interpolation=interp)
+                else:
+                    self.image = np.array(
+                        Image.fromarray(self.image).resize(
+                            (1280, 720),
+                            resample=interp
+                        )
+                    )
 
             if self.config.Emulator_ScreenshotDedithering:
                 # This will take 40-60ms
